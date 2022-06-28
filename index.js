9b3fa040445713971fb242ed67d6d6273fc86768
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-06-28 20:52:41 +0500

    files

---
+++ index.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    let a = prompt("Введите первое число:");
+    let b = prompt("Введите второе число:"); 
+    return Number(a) + Number(b);
+
+
+}
+let result = sum(a, b);
+alert(`Сумма чисел: ${result}`);
+
